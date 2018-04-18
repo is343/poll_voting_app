593377fa8e412de89a4aefde17bb5bafe1db6f7f
Ivan Schurawel <anz17n@gmail.com>
2018-04-19 00:44:06 +0900

    add ability for middleware to pass data from token

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,16 @@
 import jwt from 'jsonwebtoken';
 
+import * as db from '../models';
 
 export function loginRequired (req, res, next) {
   try {
-    var token = req.headers.authorization.split(" ")[1]
-    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+    const token = req.headers.authorization.split(" ")[1]
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
+      console.log(decoded);
       if (decoded) {
+        // passing this on to the route
+        res.locals.userId = decoded.userId;
+        res.locals.username = decoded.username;
         next();
       } else {
         res.status(401).json({ message: 'Please log in first' })
@@ -18,16 +23,22 @@ export function loginRequired (req, res, next) {
 
 export function ensureCorrectUser (req, res, next) {
   try {
-    var token = req.headers.authorization.split(" ")[1]
-    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
-      if (decoded && decoded.userId === req.params.id) {
-        next();
-      } else {
-        res.status(401).json({ message: 'Unauthorized' })
-      }
-    });
+    const token = req.headers.authorization.split(" ")[1]
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
+      db.Poll.findById(req.params.pollId, (err, foundPoll) => {
+        if (err || !foundPoll) {
+          return res.status(404).json(err);
+        }
+        if (decoded && foundPoll.userId.equals(decoded.userId)) {
+          next();
+        } else {
+          res.status(401).json({ message: 'Unauthorized' })
+        }
+        });
+      });
   } catch (e) {
     res.status(401).json({ message: 'Unauthorized' })
   }
 }
+
 
