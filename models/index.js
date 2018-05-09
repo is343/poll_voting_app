59cff7e45f0838b658f40f8abd87bce288f59a4f
Ivan Schurawel <anz17n@gmail.com>
2018-05-09 22:15:51 +0900

    remove seedDb fxn and init dotenv

--- models/index.js
+++ models/index.js
@@ -1,52 +1,32 @@
-'use strict';
+"use strict";
 
-import mongoose from 'mongoose';
+import dotenv from "dotenv";
+dotenv.config();
+
+import mongoose from "mongoose";
 
 // SETUP
-mongoose.set('debug', true);
+mongoose.set("debug", true);
 mongoose.Promise = global.Promise;
 
 // CONNECT
-mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/voting', (error) => {
-  if (error) {
-    console.error('Please make sure Mongodb is installed and running!'); // eslint-disable-line no-console
-    throw error;
-  }
-
-  // feed some dummy data in DB.
-  // dummyData();
-}, {
+mongoose.connect(
+  process.env.MONGO_URI || "mongodb://localhost/voting",
+  error => {
+    if (error) {
+      console.error("Please make sure Mongodb is installed and running!"); // eslint-disable-line no-console
+      throw error;
+    }
+  },
+  {
     keepAlive: true,
     reconnectTries: Number.MAX_VALUE,
     useMongoClient: true
-  });
-
-function dummyData() {
-  User.count().exec((err, count) => {
-    if (count > 0) {
-      return;
-    }
-    const users = [
-      { firstName: 'John', lastName: 'Doe' },
-      { firstName: 'Brad', lastName: 'Traversy' },
-      { firstName: 'Mary', lastName: 'Swanson' },
-    ];
-
-    User.create(users, (err, user) => {
-      if (err) {
-        console.error('error...', err);
-      } else {
-        console.log('new customer...', user);
-      }
-    }
-    );
-  });
-}
+  }
+);
 
 // IMPORT / EXPORT MODELS
-import User from './user';
-import Poll from './poll';
-
+import User from "./user";
+import Poll from "./poll";
 
 export { User, Poll };
-
